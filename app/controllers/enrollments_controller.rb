b7bf95bac63fd4e28137b0e2c506080aa48951bb
Ben Pritchard <ben.s.pritchard@gmail.com>
2015-07-09 15:18:02 -0700

    Add alert reporting.

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -10,6 +10,22 @@ class EnrollmentsController < ApplicationController
   end
 
   def create
-    
+    @enrollment = Enrollment.build(enrollment_params)
+
+    respond_to do |format|
+      if @enrollment.save
+        format.html { redirect_to @enrollment, notice: 'Enrollment was successfully created.' }
+        format.json { render :show, status: :created, location: @enrollment }
+      else
+        format.html { render :new }
+        format.json { render json: @enrollment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def enrollment_params
+    params.require(:enrollment).permit(:subjId, :enrollment_state_id, :project_id)
   end
 end
