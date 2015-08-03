c06cd6546ecb946d5526d5aaf4385388aac63bee
Ben Pritchard <ben.s.pritchard@gmail.com>
2015-08-03 16:20:31 -0700

    Delete more enrollments from sql file.

--- db/migrate/20150712025504_remove_unused_columns.rb
+++
@@ -1,9 +0,0 @@
-class RemoveUnusedColumns < ActiveRecord::Migration
-  def change
-    remove_column :enrollments, :status
-    remove_column :enrollments, :projId
-    remove_column :enrollments, :eligibility
-    remove_column :enrollments, :secondary
-    remove_column :enrollments, :idx
-  end
-end
