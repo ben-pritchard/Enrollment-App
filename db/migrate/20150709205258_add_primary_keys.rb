c376e570733cdb177eb884f594ff4850eb15cb51
Ben Pritchard <ben.s.pritchard@gmail.com>
2015-08-03 08:57:28 -0700

    Add migration files for production migration.

---
+++ db/migrate/20150709205258_add_primary_keys.rb
@@ -0,0 +1,9 @@
+class AddPrimaryKeys < ActiveRecord::Migration
+  def change
+    add_column :eligibility_states, :id, :primary_key
+    add_column :eligibility_sub_states, :id, :primary_key
+    add_column :enrollment_states, :id, :primary_key
+    add_column :project_list, :id, :primary_key
+    add_column :enrollment, :id, :primary_key
+  end
+end
